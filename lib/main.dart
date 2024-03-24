3542474f4ee348d590a4b11e2167b0634d03ede8
jarabey <jasminarabeysantos@gmail.com>
2024-03-23 22:41:10 -0600

    New Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:device_preview/device_preview.dart';
@@ -7,6 +9,12 @@ import 'package:device_preview/device_preview.dart';
 import 'pages/weather_page.dart';
 
 void main() {
+  DevicePreview(enabled: true,
+  tools: const [
+    ...DevicePreview.defaultTools
+  ],
+  builder: (context) => const WeatherPage()
+  )
   runApp(const MyWidget()); // Wrap your app
 }
 
